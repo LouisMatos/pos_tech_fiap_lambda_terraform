237eac86ba6883f130d2084b04fdb63fd26508de
LouisMatos <luismatos000@gmail.com>
2024-03-03 17:17:19 -0300

    ajustando campo de região

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Application Environment, such as dev, prod"
+}
+
+variable "aws_profile" {
+  type        = string
+  default     = "app_deployment_dev"
+  description = "AWS profile which is used for the deployment"
+}
