c6b661077fdd7db15e780f5673f1e47301c6fe16
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-03-07 21:58:55 -0300

    teste

--- main.tf
+++ main.tf
@@ -14,12 +14,8 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_role"
+  name               = "lambda_role_${github.run_id}"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
-
-  lifecycle {
-    ignore_changes = [assume_role_policy]
-  }
 }
 
 data "aws_iam_policy_document" "lambda" {
